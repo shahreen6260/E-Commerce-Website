cd5be4d45e1b20ce3941d59e4a238e70e05cf57f
shahreen <shahreen999shaikh@gmail.com>
2022-04-04 15:03:25 +0530

    'userDeshboardAdded'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDashboardComponent } from './userComponents/user-dashboard/user-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDashboardComponent
   ],
   imports: [
     BrowserModule,
